4e3dc223760d56b2713af02fb625edfc043a740b
Jibril Frej <80546506+Jibril-Frej@users.noreply.github.com>
2021-10-28 10:32:18 +0200

    Add files via upload

--- main.R
+++ main.R
@@ -13,6 +13,9 @@ rawCountsFile <- "RawCounts.xlsx"
 sampleDataFile <- "SampleData.xlsx"
 Compare <- "Disease"
 threshold <- 5
+saveFolder <- "Results"
+
+dir.create(saveFolder)
 
 k <- keys(hgu133plus2.db,keytype="PROBEID")
 mapping<-AnnotationDbi::select(hgu133plus2.db, keys=k, columns=c("SYMBOL"), keytype="PROBEID")
@@ -62,13 +65,13 @@ ggplot(dataGG, aes(PC1, PC2)) +
   coord_fixed(ratio = sd_ratio) +
   scale_shape_manual(values = c(4,15))
 
-ggsave("PCA.pdf")
+ggsave(file.path(saveFolder, "PCA.pdf"))
 
 ggplot(stack(data.frame(exp_raw)), aes(x = ind, y = values, fill=ind)) +
   xlab("") +ylab("")  + theme(legend.position = "none") + geom_boxplot(outlier.size = 0.1) +  
   labs(title="Boxplot of log2-intensitites for the raw data") 
 
-ggsave("boxplot_log2_intensities.pdf")
+ggsave(file.path(saveFolder, "boxplot_log2_intensities.pdf"))
 
 my_deseq2Data <- DESeqDataSetFromMatrix(countData=rawCounts, colData=sampleData, design= ~ Disease)
 my_deseq2Data <- my_deseq2Data[rowSums(counts(my_deseq2Data)) > threshold, ]
@@ -88,7 +91,7 @@ ggplot2::ggplot(RLE_data_gathered, aes(x = sample_array, y = log_expr_dev, fill=
   coord_cartesian(ylim = quantile(RLE_data_gathered$log_expr_dev, c(0.05, 0.95))) + 
   labs(title="Boxplot of log2-expression deviation")
 
-ggsave("boxplot_log2_expression_deviation.pdf")
+ggsave(file.path(saveFolder, "boxplot_log2_expression_deviation.pdf"))
 
 exp <- counts(my_deseq2Data, normalized=TRUE)
 PCA <- prcomp(t(exp), scale = FALSE)
@@ -111,7 +114,7 @@ ggplot(dataGG, aes(PC1, PC2)) +
   coord_fixed(ratio = sd_ratio) +
   scale_shape_manual(values = c(4,15))
 
-ggsave("PCA_normalized.pdf")
+ggsave(file.path(saveFolder, "PCA_normalized.pdf"))
 
 annotation_for_heatmap <- data.frame("tmp" = sampleData[,Compare])
 colnames(annotation_for_heatmap) = c(Compare)
@@ -132,11 +135,11 @@ pheatmap(dists, col = (hmcol),
                            max(dists, na.rm = TRUE)), 
          legend_labels = (c("small distance", "large distance")),
          main = "Clustering heatmap",
-         filename="heatmap.pdf")
+         filename=file.path(saveFolder, "heatmap.pdf"))
 
 medians <- rowMedians(log2(exp))
 
-pdf("hist_median_intensities.pdf")
+pdf(file.path(saveFolder, "hist_median_intensities.pdf"))
 hist_res <- hist(medians, 100, col = "cornsilk", freq = FALSE,
                  main = "Histogram of the median intensities",
                  border = "antiquewhite4",
@@ -150,7 +153,7 @@ my_deseq2Results <- results(my_deseq2Data, contrast=c(Compare, levels(sampleData
 deseq2ResDF <- as.data.frame(my_deseq2Results)
 deseq2ResDF <- add_column(deseq2ResDF, SYMBOL = mapping[rownames(deseq2ResDF),], .after = 0)
 deseq2ResDF <- add_column(deseq2ResDF, PROBEID = rownames(deseq2ResDF), .after = 0)
-write_xlsx(deseq2ResDF, "DiffExprRes.xlsx")
+write_xlsx(deseq2ResDF, file.path(saveFolder, "DiffExprRes.xlsx"))
 
 deseq2ResDF$significant <- ifelse(deseq2ResDF$padj < 0.1, "Significant", NA)
 
@@ -166,7 +169,7 @@ ggplot(deseq2ResDF, aes(baseMean, log2FoldChange, colour=padj)) +
   labs(x="mean of normalized counts", y="log fold change") +
   scale_colour_viridis(direction=-1, trans='sqrt') + theme_bw()
 
-ggsave("fold_vs_count.pdf")
+ggsave(file.path(saveFolder, "fold_vs_count.pdf"))
 
 ggplot(deseq2ResDF, aes(log2FoldChange, baseMean, colour=significant)) +
   geom_point(size=1) + scale_x_continuous(limits=c(-3, 3), oob=squish) +
@@ -180,7 +183,7 @@ ggplot(deseq2ResDF, aes(log2FoldChange, baseMean, colour=padj)) +
   labs(y="mean of normalized counts", x="log2 fold change") +
   scale_colour_viridis(direction=-1, trans='sqrt') + theme_bw()
 
-ggsave("count_vs_fold.pdf")
+ggsave(file.path(saveFolder, "count_vs_fold.pdf"))
 
 logdeseq2ResDF = deseq2ResDF
 logdeseq2ResDF$padj = -log10(logdeseq2ResDF$padj)
@@ -189,4 +192,4 @@ ggplot(logdeseq2ResDF, aes(log2FoldChange, padj)) +
   geom_point(size=1) + scale_x_continuous(limits=c(-3, 3), oob=squish) +
   labs(y="-log10 P-value", x="log2 fold change")
 
-ggsave("volcano.pdf")
+ggsave(file.path(saveFolder, "volcano.pdf"))
